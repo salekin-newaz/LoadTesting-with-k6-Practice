17b0d5a3b43d21073528904c01e9b124a11d8c8f
Md Salekin Newaz <52202484+salekin-newaz@users.noreply.github.com>
2023-08-11 01:23:16 +0600

    add virtual user

--- sample.js
+++ sample.js
@@ -1,10 +1,17 @@
 import http from 'k6/http';
-import { sleep } from 'k6';
+import { check, sleep } from 'k6';
+
+export const options = {
+  vus: 10,
+  iteration: 10,
+};
+
 
 export default function () {
     const baseUrl = "https://reqres.in/api/users?page=2"
     const endPoint = "api/users/2";
     const res = http.get(`${baseUrl}${endPoint}`);
+    check(res, {'is status 200': (r) => r.status === 200,});
     console.log(res);
     sleep(1);
 }
