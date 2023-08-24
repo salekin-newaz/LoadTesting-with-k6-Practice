be71213416a5fb6de538b581f77e229f4afcbb8a
Md Salekin Newaz <52202484+salekin-newaz@users.noreply.github.com>
2023-08-24 20:07:20 +0600

    practice with executor

--- sample.js
+++ sample.js
@@ -1,17 +1,59 @@
 import http from 'k6/http';
-import { check, sleep } from 'k6';
+import {sleep} from 'k6';
+import exec from "k6/execution";
 
-export const options = {
+export const users= JSON.parse(open("data.json"))
+/*export const options = {
   vus: 10,
   iteration: 10,
 };
+*/
+
+export const options = {
+   scenarios:{
+    //    accountCreate:{
+    //        executor:"per-vu-iterations",
+    //        vus:1,
+    //        iterations:10
+
+    //    },
+    //    accountCreate2:{
+    //        executor:"shared-iterations",
+    //        vus:5,
+    //        iterations:10
+       
+    //    }
+
+         accountCreate3: {
+            executor:"constant-vus",
+            vus: 10000,
+            duration: '1s'
+         }
+
+        // accountCreate3: {
+        //     executor:"ramping-vus",
+        //     startVUs: 0,
+        //     stages:[
+        //         {duration:'20s', target:10},
+        //         {duration: '30s', target:10},
+        //         {duration: '0s', target:0},
+        //     ],
+        //         gracefulRampDown: '30s'
+        
+
+        //  }
+   }
+}
 
 
 export default function () {
+    //console.log(exec.vu.idInTest);
+    console.info(`Iterations id ==>  ${exec.scenario.iterationInTest} VU id ==> ${exec.vu.idInTest}`);
+
     const baseUrl = "https://reqres.in/api/users?page=2"
     const endPoint = "api/users/2";
     const res = http.get(`${baseUrl}${endPoint}`);
     check(res, {'is status 200': (r) => r.status === 200,});
     console.log(res);
-    sleep(1);
+     sleep(1);
 }
